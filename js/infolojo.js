8a1b725e366d54bd8b4ccda455c965f7f8205eac
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-04-11 20:27:32 +0200

    finish

--- js/infolojo.js
+++ js/infolojo.js
@@ -1,13 +1,35 @@
 
-
 const menuMobileTag = document.querySelector('#menu-mobile')
-let navMobile = 1
+const htmlGeneral = document.querySelector('#general')
+let navMobile = -1
+let accesiblemode = -1
+
+
+document.onload = () => {
+    
+
+    menuMobile()
+}
+
+
 
 /**
  * Show an hidde menu
  */
 const menuMobile = () => {
+    navMobile *= -1
+    navMobile===-1?menuMobileTag.style.display="none":menuMobileTag.style.display = "block"
 
-    navMobile===1?menuMobileTag.style.display="none":menuMobileTag.style.display = "block"
+}
+
+
+/**
+ * change size
+ */
+const changeSize = () => {
+    accesiblemode *= -1
+    console.log(accesiblemode)
+    accesiblemode===1?htmlGeneral.classList.add('font-accesible'):htmlGeneral.classList.remove('font-accesible')
+
 }
     
