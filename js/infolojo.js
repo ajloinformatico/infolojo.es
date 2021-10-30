68cbd39856dcdce213f38cddbbe2c5c77ed7c09f
ajloinformatico <ajloinformatico@gmail.com>
2021-10-30 12:06:14 +0200

    href sections changed to js scroll anims
    small refactor of js file

--- js/infolojo.js
+++ js/infolojo.js
@@ -1,22 +1,31 @@
+const body = document.body;
+const menuMobileTag = document.querySelector('#menu-mobile');
+const htmlGeneral = document.querySelector('#general');
+const btnScrollToTop = document.querySelector('#scrollTop');
+const TIME_ANIMATION_SCROLL_TOP = 1000;
+let firstLoad = true;
+let navMobile = 1;
+let accesiblemode = -1;
 
-const menuMobileTag = document.querySelector('#menu-mobile')
-const htmlGeneral = document.querySelector('#general')
-const TIME_ANIMATION_SCROLL_TOP = 1000
-let navMobile = 1
-let accesiblemode = -1
+/**
+ * load state methods 
+ */
+window.onload = () => {
+    console.log("run all");
+    //hidde btn to scroll up and menu btn
+    btnScrollToTop.style.display = "none";
+    menuMobile();
+    scrollbtn();
+}
 
-
-/** Init menu bottom to scroll up or scroll down*/
-
-$(document).ready(() => {
-
-    document.querySelector('#scrollTop').style.display = "none"
-    menuMobile()
-
+/**
+ * Make ScrollToTop
+ */
+const scrollbtn = () => {
     $(window).scroll(() => {
         // if scroll > 200 muestra el botom si no ocultalo
         if ($(this).scrollTop() > 200) {
-            $('#scrollTop').fadeIn()
+            $('#scrollTop').fadeIn();
         
         } else {
             $('#scrollTop').fadeOut();
@@ -26,30 +35,46 @@ $(document).ready(() => {
     $('#scrollTop').click(() => {
         $('html, body').animate({
             scrollTop:0
-        },TIME_ANIMATION_SCROLL_TOP)
-        return false
+        },TIME_ANIMATION_SCROLL_TOP);
+        return false;
     });
-
-});
-
-
-/**
- * Show an hidde menu
- */
-const menuMobile = () => {
-    navMobile *= -1
-    navMobile===-1?menuMobileTag.style.display="none":menuMobileTag.style.display = "block"
-
 }
 
+/**
+ * trigger scroll to top btn
+ */
+const triggerScrollTop = () => {
+    $('html, body').animate({
+        scrollTop:0
+    },TIME_ANIMATION_SCROLL_TOP);
+    return false;
+}
+
+/**
+ * hide menu on start 
+ */
+const menuMobile = () => {
+   navMobile *= -1;
+   navMobile===-1?menuMobileTag.style.display="none":menuMobileTag.style.display = "block";
+
+}
 
 /**
  * change size
  */
 const changeSize = () => {
-    accesiblemode *= -1
-    console.log(accesiblemode)
-    accesiblemode===1?htmlGeneral.classList.add('font-accesible'):htmlGeneral.classList.remove('font-accesible')
-
+    accesiblemode *= -1;
+    console.log(accesiblemode);
+    accesiblemode===1?htmlGeneral.classList.add('font-accesible'):htmlGeneral.classList.remove('font-accesible');
 }
-    
+
+/**
+ * anim go to id element on the web
+ * @param {string} element 
+ * @returns {void}
+ */
+const navTo = (element) => {
+    $('html, body').animate({
+        scrollTop:$(element).offset().top
+    },TIME_ANIMATION_SCROLL_TOP);
+}
